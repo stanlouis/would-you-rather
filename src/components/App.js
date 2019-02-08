c2bfbd97d25f33044b4962201fcdb2f4ac756a99
Stanley Louis <stanlouis@msn.com>
2019-02-08 13:22:22 -0500

    Add starting base files and folders

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Would You Rather</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
